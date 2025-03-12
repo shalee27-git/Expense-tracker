2b4fe41eab8a3df45ee494d225333eab6d89cf1f
Shiamilee <srishyam4@gmail.com>
2025-03-12 17:34:10 +0530

    Project testing completed successfully

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TrackingHistory, CurrentBalance
+from django.db.models import Sum
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+
+        current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+        expense_type = "CREDIT"
+
+        if float(amount) < 0:
+            expense_type = "DEBIT"
+
+
+        tracking_history = TrackingHistory.objects.create(amount = amount, expense_type = expense_type, 
+                                                           description = description, current_balance = current_balance)
+        current_balance.current_balance += float(tracking_history.amount)
+        current_balance.save()
+
+        return redirect('/')
+    
+    current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+    income = 0
+    expense = 0
+
+    for tracking_history in TrackingHistory.objects.all():
+        if tracking_history.expense_type == "CREDIT":
+            income += tracking_history.amount
+        else:
+            expense += tracking_history.amount
+
+    context = {'income': income, 'expense': expense,'transactions': TrackingHistory.objects.all(), 'current_balance': current_balance}
+    return render(request, 'index.html', context)
+
+
+
+def delete_transaction(request, id):
+    tracking_history = TrackingHistory.objects.filter(id = id)
+    if tracking_history.exists:
+        current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+        tracking_history = tracking_history[0]
+        current_balance.current_balance -= tracking_history.amount
+        current_balance.save()
+
+        tracking_history.delete()
+
+    return redirect('/')
